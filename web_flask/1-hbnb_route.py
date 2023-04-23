d9aff5b7bfa1b53df55d8acaf705efd81d289c2f
Jorshstar <Jorshstar>
2023-04-23 16:14:41 -0700

    toes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,23 @@
-
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-"""
+"""Create A web application using flask"""
+
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+@app.route('/')
+def index():
+    """create an  index page for my web application"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """Displays 'HBNB'."""
+    """ Create an additional route to my web application"""
     return "HBNB"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000))
